24d457d42714de99cb5a804e69e3eb7685122bbf
tony-coder <1606771552@qq.com>
2020-03-31 10:52:30 +0800

    完善配置文件

--- Chapter_05/Spittr/src/main/java/spittr/config/RootConfig.java
+++ Chapter_05/Spittr/src/main/java/spittr/config/RootConfig.java
@@ -1,4 +1,15 @@
 package spittr.config;
 
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = {"spittr"},  // 扫描spittr包下，不扫描带有@EnableWebMvc注解的配置文件
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)
+        })
 public class RootConfig {
 }
--- Chapter_05/Spittr/src/main/java/spittr/controller/WebConfig.java
+++ Chapter_05/Spittr/src/main/java/spittr/controller/WebConfig.java
@@ -1,4 +1,37 @@
 package spittr.controller;
 
-public class WebConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * DispatcherServlet的配置声明
+ */
+
+@Configuration
+@EnableWebMvc  // 启用SpringMVC
+@ComponentScan("spittr.controller")  // 启用组件扫描 范围是spittr.controller包下
+public class WebConfig implements WebMvcConfigurer {
+    // 配置JSP视图解析器
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("*/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        resolver.setExposeContextBeansAsAttributes(true);
+        return resolver;
+    }
+
+    // 配置静态资源的处理
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        // 通过调用DefaultServletHandlerConfigurer的enable()方法，我们要求DispatcherServlet将对静态资源的请求转发到Servlet容器中默认的Servlet上
+        // 而不是使用DispatcherServlet本身来处理此类请求
+        configurer.enable();
+    }
 }
