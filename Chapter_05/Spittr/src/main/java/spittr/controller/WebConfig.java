f145c976f76f207d39a113e062f0e896225512ae
tony-coder <1606771552@qq.com>
2020-03-31 23:35:26 +0800

    添加基础控制器 HomeController

--- Chapter_05/Spittr/src/main/java/spittr/controller/WebConfig.java
+++ Chapter_05/Spittr/src/main/java/spittr/controller/WebConfig.java
@@ -21,7 +21,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("*/WEB-INF/views/");
+        resolver.setPrefix("/WEB-INF/views/");
         resolver.setSuffix(".jsp");
         resolver.setExposeContextBeansAsAttributes(true);
         return resolver;
